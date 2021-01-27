75aa1fa2c110b74a35cfc7a02559351670080dd7
celine92-cyber <71047579+celine92-cyber@users.noreply.github.com>
2021-01-27 10:39:00 -0700

    update3

--- wwwroot/js/calculator.js
+++ wwwroot/js/calculator.js
@@ -1,6 +1,4 @@
-﻿//function calc() will automatically starts when user submit the form
-
-function calc() {
+﻿$("#submit").click(function () {
     // define variables
     var assignmentsScore;
     var groupProjectsScore;
@@ -11,11 +9,11 @@ function calc() {
     var letterGrade;
 
     //get data
-    assignmentsScore = parseFloat(document.getElementById("assignments").value);
-    groupProjectsScore = parseFloat(document.getElementById("groupProjects").value);
-    quizzesScore = parseFloat(document.getElementById("quizzes").value);
-    examsScore = parseFloat(document.getElementById("exams").value);
-    intexScore = parseFloat(document.getElementById("intex").value);
+    assignmentsScore = parseFloat($("#assignments").val());
+    groupProjectsScore = parseFloat($("#groupProjects").val());
+    quizzesScore = parseFloat($("#quizzes").val());
+    examsScore = parseFloat($("#exams").val());
+    intexScore = parseFloat($("#intex").val());
     finalGrade = assignmentsScore * 0.5 + groupProjectsScore * 0.1 + quizzesScore * 0.1 + examsScore * 0.2 + intexScore * 0.1;
     Math.round(finalGrade);
 
@@ -34,4 +32,4 @@ function calc() {
     else { letterGrade = "E"; }
 
     alert("Your final numeric grade is " + finalGrade + " and your final letter grade is " + letterGrade + ".")
-}
+});
