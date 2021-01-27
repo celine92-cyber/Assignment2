5c8b4cd69c920481e5c0fea13b4009a717fefbf1
celine92-cyber <71047579+celine92-cyber@users.noreply.github.com>
2021-01-26 23:40:56 -0700

    update1

---
+++ Controllers/Hw2Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Controllers
+{
+    public class Hw2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator ()
+        {
+            return View();
+        }
+    }
+}
